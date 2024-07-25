05e2d5c60d87601936ccf9371db6270906a61f56
Resul Elezi <resul_elezi@protonmail.com>
2024-07-25 11:47:36 +0200

    #

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
+const resultEl = document.getElementById("result");
 
 function checkInput() {
     if(textInput.value === "") {
         alert('Please input a value');
+    return;
+    } else if(textInput.value === "A") {
+      resultEl.innerHTML =  "A is a palindrome";
     return;
     }
 };
