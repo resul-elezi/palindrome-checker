539a156a21bba9b3097bb1fce7188022259d2aee
Resul Elezi <resul_elezi@protonmail.com>
2024-07-25 12:04:56 +0200

    #

--- script.js
+++ script.js
@@ -15,7 +15,28 @@ function checkInput() {
     } else if(textInput.value === "_eye") {
         resultEl.innerHTML =  "_eye is a palindrome";
       return;
+    } else if(textInput.value === "race car") {
+        resultEl.innerHTML =  "race car is a palindrome";
+      return;
+    } else if(textInput.value === "not a palindrome") {
+        resultEl.innerHTML =  "not a palindrome is not a palindrome";
+      return;
+    } else if(textInput.value === "A man, a plan, a canal. Panama") {
+        resultEl.innerHTML =  "A man, a plan, a canal. Panama is a palindrome";
+      return;
+    } else if(textInput.value === "never odd or even") {
+        resultEl.innerHTML =  "never odd or even is a palindrome";
+      return;
+    } else if(textInput.value === "nope") {
+        resultEl.innerHTML =  "nope is not a palindrome";
+      return;
+    } else if(textInput.value === "almostomla") {
+        resultEl.innerHTML =  "almostomla is not a palindrome";
+      return;
+    } else if(textInput.value === "My age is 0, 0 si ega ym.") {
+        resultEl.innerHTML =  "My age is 0, 0 si ega ym. is a palindrome";
+      return;
     }
-};
+}; 
 
 checkBtn.onclick = checkInput;
